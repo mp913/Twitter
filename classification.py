309d9fb3a7b143259d6fd4832589fcea33fcf4c1
mp913 <zarkov01@mail.ru>
2019-12-24 14:18:30 +0300

    Finish classification

--- classification.py
+++ classification.py
@@ -1,4 +1,6 @@
 import dostoevsky
+from dostoevsky.tokenization import RegexTokenizer
+from dostoevsky.models import FastTextSocialNetworkModel
 
 inp = open("clear_data.txt", "r", encoding='UTF8')
 in_dict = open("estimations.txt", "r", encoding='UTF8')
@@ -75,6 +77,84 @@ for line in inp:
 
 counter = good_counter + bad_counter + normal_counter
 print("Rule 2: percentage of ratings", file=outp)
+print("Good - " + str(good_counter) + " - " + str(round(100*good_counter/counter)) + "%", file=outp)
+print("Bad - " + str(bad_counter) + " - " + str(round(100*bad_counter/counter)) + "%", file=outp)
+print("Neutral - " + str(normal_counter) + " - " + str(round(100*normal_counter/counter)) + "%", file=outp)
+print("", file=outp)
+
+# классификация по третьему правилу - обнаружили 3 или более положительных слов = твит хороший
+good_counter = 0
+bad_counter = 0
+normal_counter = 0
+
+inp = open("clear_data.txt", "r", encoding='UTF8')
+for line in inp:
+    line = line.replace("\n", " ")
+    if line == " ":
+        continue
+    arr = line.split(' ')
+
+    good_word_counter = 0
+    bad_word_counter = 0
+    normal_word_counter = 0
+
+    for word in arr:
+        if word in e_dictionary:
+            if e_dictionary[word] == -1:
+                bad_word_counter += 1
+            elif e_dictionary[word] == 1:
+                good_word_counter += 1
+            elif e_dictionary[word] == 0:
+                normal_word_counter += 1
+
+    if good_word_counter > 2:
+        good_counter += 1
+    elif bad_word_counter > good_word_counter:
+        bad_counter += 1
+    else:
+        normal_counter += 1
+
+counter = good_counter + bad_counter + normal_counter
+print("Rule 3: 3 good words", file=outp)
+print("Good - " + str(good_counter) + " - " + str(round(100*good_counter/counter)) + "%", file=outp)
+print("Bad - " + str(bad_counter) + " - " + str(round(100*bad_counter/counter)) + "%", file=outp)
+print("Neutral - " + str(normal_counter) + " - " + str(round(100*normal_counter/counter)) + "%", file=outp)
+print("", file=outp)
+
+# классификация по четвертому правилу - хорошие - твиты без плохих слов, плозие - без хороших
+good_counter = 0
+bad_counter = 0
+normal_counter = 0
+
+inp = open("clear_data.txt", "r", encoding='UTF8')
+for line in inp:
+    line = line.replace("\n", " ")
+    if line == " ":
+        continue
+    arr = line.split(' ')
+
+    good_word_counter = 0
+    bad_word_counter = 0
+    normal_word_counter = 0
+
+    for word in arr:
+        if word in e_dictionary:
+            if e_dictionary[word] == -1:
+                bad_word_counter += 1
+            elif e_dictionary[word] == 1:
+                good_word_counter += 1
+            elif e_dictionary[word] == 0:
+                normal_word_counter += 1
+
+    if bad_word_counter == 0:
+        good_counter += 1
+    elif good_word_counter == 0:
+        bad_counter += 1
+    else:
+        normal_counter += 1
+
+counter = good_counter + bad_counter + normal_counter
+print("Rule 4: availability of good/bad words", file=outp)
 print("Good - " + str(good_counter) + " - " + str(round(100*good_counter/counter)) + "%", file=outp)
 print("Bad - " + str(bad_counter) + " - " + str(round(100*bad_counter/counter)) + "%", file=outp)
 print("Neutral - " + str(normal_counter) + " - " + str(round(100*normal_counter/counter)) + "%", file=outp)
